53636d77795899a6a0fce9675014279f1ab22264
AlexNem <darkoffalex@yandex.ru>
2025-05-28 18:05:52 +0300

    Demo resources clean-up
    Demo resources clean-up (pt.2)

---
+++ ModelsSH/modular_industrial_pipes_01_1k/02/segment_08_GRH.h
@@ -0,0 +1,13 @@
+// Animation names
+#define SEGMENT_08_GRH_ANIM_DEFAULT 0
+
+// Color names
+
+// Patch names
+
+// Names of collision boxes
+#define SEGMENT_08_GRH_COLLISION_BOX_PART_NAME 0
+
+// Attaching position names
+
+// Sound names
---
+++ ModelsSH/painted_wooden_cabinet_1k/painted_wooden_cabinet_1k_GR.h
@@ -0,0 +1,13 @@
+// Animation names
+#define PAINTED_WOODEN_CABINET_1K_GR_ANIM_DEFAULT 0
+
+// Color names
+
+// Patch names
+
+// Names of collision boxes
+#define PAINTED_WOODEN_CABINET_1K_GR_COLLISION_BOX_PART_NAME 0
+
+// Attaching position names
+
+// Sound names
